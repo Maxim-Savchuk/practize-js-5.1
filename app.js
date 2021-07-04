a916e12adf69fabcc775b8feed7509362b463685
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-04 18:14:52 +0300

    done

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const exhbs = require('express-handlebars');
+const products = require('./products.json');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+    'hbs',
+    exhbs({
+    extname: 'hbs'
+    }),
+);
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        cssFileName: 'home',
+        pageTitle: 'Гланая страница',
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        cssFileName: 'about',
+        pageTitle: 'О нас',
+    });
+})
+
+app.get('/products', (req, res) => {
+    res.render('products', {
+        products,
+        cssFileName: 'products',
+        pageTitle: 'Товары',
+    });
+})
+
+app.get('/product/:productId', (req, res) => {
+    const product = products.find(p => p.id === req.params.productId);
+
+    res.render('product', { product, cssFileName: 'product' });
+})
+
+app.listen(4444, () => {
+    console.log(`Application server is running on port ${4444}`);
+});
